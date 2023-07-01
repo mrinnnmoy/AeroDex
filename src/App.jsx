d6eefd68dd371df929cad3a4e9273209300e5ab1
mrinnnmoy <mrinmoy202000@gmail.com>
2023-07-01 20:18:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Logo from "./assets/ticket.png";
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <img src={Logo} alt="header-logo" />
+      <h1>Welcome to AeroDex</h1>
+      <button>Connect</button>
+    </div>
+    </>
+  )
+}
+
+export default App
