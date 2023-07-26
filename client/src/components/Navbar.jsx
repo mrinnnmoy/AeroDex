533237c79eef70c6e290b7e487eef7be2c45e4ed
mrinnnmoy <mrinmoy202000@gmail.com>
2023-07-26 20:31:47 +0530

    update navbar-connectWalletBtn

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ethers } from 'ethers';
 
 const Navbar = () => {
+
+  const [provider, setProvider] = useState(null);
+  const [connectedAddress, setConnectedAddress] = useState("");
+
+  const connectWallet = async () => {
+    try {
+      if (window.ethereum) {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        setProvider(provider);
+        const address = await signer.getAddress();
+        setConnectedAddress(address);
+      } else {
+        alert("Metamask not found. Please install Metamask to continue.");
+      }
+    } catch (error) {
+      console.error("Error connecting to wallet:", error);
+    }
+  };
+
   return (
     <div className='nav'>
       <div className="nav-left">
@@ -8,10 +30,8 @@ const Navbar = () => {
       </div>
 
       <div className="nav-right">
-        <button className='btn'>
-          <a href="./">
-            Mint
-          </a>
+        <button className='btn' onClick={connectWallet}>
+          Connect
         </button>
       </div>
     </div>
